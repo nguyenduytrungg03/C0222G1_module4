de8bb1d1116934181abc5b55949ae5b3d99cfa19
nguyenduytrungg03 <95289035+nguyenduytrungg03@users.noreply.github.com>
2022-06-21 22:00:25 +0800

    fix btap

--- ss3_views_thymeleaf/exercise/Product Management/src/main/java/com/product/controller/ProductController.java
+++ ss3_views_thymeleaf/exercise/Product Management/src/main/java/com/product/controller/ProductController.java
@@ -5,10 +5,7 @@ import com.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ProductController {
@@ -17,7 +14,7 @@ public class ProductController {
 
     @RequestMapping("/")
     public String home(Model model) {
-        model.addAttribute("product",new Product());
+        model.addAttribute("product", new Product());
         model.addAttribute("listProduct", productService.findAll());
         return "list";
     }
@@ -29,38 +26,40 @@ public class ProductController {
     }
 
     @PostMapping("/save")
-    public String save(Product product){
-        productService.save(product);
+    public String save(@ModelAttribute("product") Product product, Model model) {
+        model.addAttribute("product", product);
+//        productService.save(product);
         return "redirect:/";
     }
 
     @GetMapping("/delete/{id}")
     public String delete(@PathVariable("id") String id) {
-         productService.remove(id);
+        productService.remove(id);
         return "redirect:/";
     }
 
 
     @GetMapping("/edit/{id}")
-    public String goFormEdit(@PathVariable String id,  Model model) {
+    public String goFormEdit(@PathVariable String id, Model model) {
         model.addAttribute("product", productService.findById(id));
         return "/edit";
     }
+
     @PostMapping("/edit")
-    public String edit(Product product){
+    public String edit(Product product) {
         productService.update(product);
         return "redirect:/";
     }
 
     @GetMapping("/view/{id}")
-    public String view(@PathVariable String id, Model model){
-        model.addAttribute("product",productService.findById(id));
+    public String view(@PathVariable String id, Model model) {
+        model.addAttribute("product", productService.findById(id));
         return "/view";
     }
 
     @GetMapping("/search")
-    public String searchByName(Product product,Model model){
-        model.addAttribute("listProduct",productService.searchByName(product.getName()));
+    public String searchByName(Product product, Model model) {
+        model.addAttribute("listProduct", productService.searchByName(product.getName()));
         return "list";
     }
 }
